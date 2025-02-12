eb686e215343f98cb7772ec99c7ed9402e1e73a7
Leonardo Borja <leoborja@MacBook-Pro-de-Leonardo.local>
2025-02-12 16:29:38 -0300

    Removendo arquivos antigos

--- src/components/Patterns/index.tsx
+++
@@ -1,5 +0,0 @@
-export { Waves } from './Waves';
-export { WideWaves } from './WideWaves';
-export { ChartsElements } from './ChartsElements';
-export { RoundedText } from './RoundedText';
-export { Lines } from './Lines';
--- src/utils/constants.ts
+++
@@ -1,5 +0,0 @@
-export const SITE_URL = {
-  en: 'https://etus.digital/en',
-  'pt-BR': 'https://etus.digital',
-  es: 'https://etus.digital/es',
-};
