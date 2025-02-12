a2c3431f0e2ba99a9e8598e25852e81c5e73463d
Leonardo Borja <leoborja@MacBook-Pro-de-Leonardo.local>
2025-02-12 17:01:58 -0300

    mudar analytics

--- src/components/GoogleAnalytics4/index.tsx
+++ src/components/GoogleAnalytics4/index.tsx
@@ -1,37 +1,54 @@
 import { useRouter } from 'next/router';
 import Script from 'next/script';
 import { memo, useEffect } from 'react';
+
 const TRACKING_ID = process.env.NEXT_PUBLIC_GA4_TRACKING_ID;
+
+declare global {
+  interface Window {
+    dataLayer?: any[];
+    gtag?: (...args: any[]) => void;
+  }
+}
 
 const GoogleAnalytics = () => {
   const router = useRouter();
+
   useEffect(() => {
     if (!TRACKING_ID || router.isPreview) return;
-    gtag('config', TRACKING_ID, {
-      send_page_view: false,
-    });
-    gtag('event', 'page_view', {
+
+    // Define gtag caso não esteja definido
+    window.gtag = window.gtag || function (...args) {
+      window.dataLayer = window.dataLayer || [];
+      window.dataLayer.push(args);
+    };
+
+    window.gtag('config', TRACKING_ID, { send_page_view: false });
+    window.gtag('event', 'page_view', {
       page_path: window.location.pathname,
       send_to: TRACKING_ID,
     });
-  }, []); // eslint-disable-line
+  }, [router]);
 
   useEffect(() => {
     const handleRouteChange = (url: string) => {
       if (!TRACKING_ID || router.isPreview) return;
 
-      gtag('event', 'page_view', {
+      window.gtag?.('event', 'page_view', {
         page_path: url,
         send_to: TRACKING_ID,
       });
     };
+
     router.events.on('routeChangeComplete', handleRouteChange);
     router.events.on('hashChangeComplete', handleRouteChange);
+
     return () => {
       router.events.off('routeChangeComplete', handleRouteChange);
       router.events.off('hashChangeComplete', handleRouteChange);
     };
   }, [router.events, router.isPreview]);
+
   if (!TRACKING_ID || router.isPreview) {
     return null;
   }
@@ -40,18 +57,22 @@ const GoogleAnalytics = () => {
     <>
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${TRACKING_ID}`}
-      ></Script>
+        strategy="afterInteractive"
+      />
       <Script
         id="gtag-init"
+        strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
+            gtag('config', '${TRACKING_ID}');
           `,
         }}
       />
     </>
   );
 };
+
 export default memo(GoogleAnalytics);
